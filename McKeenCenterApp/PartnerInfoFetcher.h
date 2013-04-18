5dc35399b17917b5a3bac41af2f34340363d4e24
Andrew Daniels <andrew.daniels714@gmail.com>
2013-04-17 22:09:33 -0400

    added PartnerInfoFetcher for partner program finder

---
+++ McKeenCenterApp/PartnerInfoFetcher.h
@@ -0,0 +1,13 @@
+//
+//  PartnerInfoFetcher.h
+//  McKeenCenterApp
+//
+//  Created by Andrew Daniels on 4/17/13.
+//  Copyright (c) 2013 Andrew Daniels and Evan Hoyt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PartnerInfoFetcher : NSObject
+
+@end
---
+++ McKeenCenterApp/PartnerInfoFetcher.m
@@ -0,0 +1,17 @@
+//
+//  PartnerInfoFetcher.m
+//  McKeenCenterApp
+//
+//  Created by Andrew Daniels on 4/17/13.
+//  Copyright (c) 2013 Andrew Daniels and Evan Hoyt. All rights reserved.
+//
+
+#import "PartnerInfoFetcher.h"
+
+@implementation PartnerInfoFetcher
+
+- (NSString *)infoForPartnerWithID:(NSInteger *) id{
+	return @"Partner\n888-888-8888";
+}
+
+@end
