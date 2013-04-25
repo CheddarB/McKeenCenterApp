d9096e36289a4d899aa03fe44fb0b30f8f17eb3d
Andrew Daniels <andrew.daniels714@gmail.com>
2013-04-25 16:02:00 -0400

    info fetcher now grabs all info (added phone number)

--- McKeenCenterApp/PartnerInfoFetcher.m
+++ McKeenCenterApp/PartnerInfoFetcher.m
@@ -40,17 +40,13 @@
 	section = [section children][7];
 	section = [section children][1];
 	section = [section children][1];
-	
-	NSString * content = [section allContents];
-	NSLog(@"%@", content);
-	
+		
 	//grab data
 	//site
 	
 	if([section children] && [[section children] count] >= 1){
 		HTMLNode * siteNode = [section children][0];
 		self.site = [siteNode allContents];
-		NSLog(@"%@",self.site);
 		
 		if([[section children] count] >= 2){
 			HTMLNode * addressNode = [section children][1];
@@ -59,27 +55,30 @@
 				//name
 				HTMLNode * nameNode = [addressNode children][0];
 				self.name = [nameNode contents];
-				NSLog(@"%@", self.name);
 				
 				if([nameNode children] && [[nameNode children] count] >= 2){
 					//street
 					HTMLNode * streetNode = [nameNode children][1];
 					self.street = [streetNode contents];
-					NSLog(@"%@", self.street);
+					
+					
 					
 					if([streetNode children] && [[streetNode children] count] >= 3){
 						//town
 						HTMLNode * townNode = [streetNode children][2];
 						self.town = [PartnerInfoFetcher fixTown:[townNode allContents]];
-						NSLog(@"%@",self.town);
 						
 						if ([[streetNode children] count] >= 5) {
 							//phone
-							HTMLNode * phoneNode = [streetNode children][4];
-							self.phone = [phoneNode allContents];
-							NSLog(@"%@", self.phone);
+							HTMLNode * emailNode = [streetNode children][4];
+							self.email = [emailNode allContents];
+							
+							if ([[streetNode children] count] >= 7) {
+								//phone
+								HTMLNode * phoneNode = [streetNode children][6];
+								self.phone = [phoneNode allContents];
+							}
 						}
-						
 					}
 				}
 			}
@@ -110,8 +109,13 @@
 					infoString = [infoString stringByAppendingString:self.town];
 					infoString = [infoString stringByAppendingString:@"\n"];
 					
-					if(self.phone){
-						infoString = [infoString stringByAppendingString:self.phone];
+					if(self.email){
+						infoString = [infoString stringByAppendingString:self.email];
+						infoString = [infoString stringByAppendingString:@"\n"];
+
+						if (self.phone) {
+							infoString = [infoString stringByAppendingString:self.phone];
+						}
 					}
 				}
 			}
@@ -122,12 +126,25 @@
 }
 
 - (NSString *)getSite{
+	NSString * site = nil;
+	
 	if(self.site){
-		return self.site;
-	}else{
-		return @"";
+		site = self.site;
 	}
+	
+	return site;
 }
+
+- (NSString *)getEmail{
+	NSString * email = nil;
+	return email;
+}
+
+- (NSString *)getPhone{
+	NSString * phone = nil;
+	return phone;
+}
+
 
 + (NSString *)fixTown:(NSString *)town{
 	NSArray * townParts = [town componentsSeparatedByString:@"\r\n"];
